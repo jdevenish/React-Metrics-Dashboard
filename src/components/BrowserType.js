8d0d4e2744d8c2e540d6e44210cec305b5173fb7
Justin Devenish <justin.devenish@gmail.com>
2020-07-08 17:33:02 +0000

    Starting to build pie chart

--- src/components/BrowserType.js
+++ src/components/BrowserType.js
@@ -1,13 +1,23 @@
 import React from "react";
+import * as d3 from "d3";
 
 export default function BrowserType({ browserTypeData }) {
-
-  // d3.select("#browser-type").style("color", "green")
+  // const [browserKeys, setBrowserkeys] = useState([]);
+  let browserKeys = [];
+  let browserValues = [];
+  d3.select("#browser-type").style("color", "green")
+  for(let key in browserTypeData) {
+    browserKeys.push(key);
+    browserValues.push(browserTypeData[key]);
+  }
+  
 
   return (
     <div className="App">
       <h1 id="browser-type">Browser Type Graph</h1>
-
+      <svg id="browser-type-pie" height="300" width="400">
+        
+      </svg>
     </div>
   );
 }
